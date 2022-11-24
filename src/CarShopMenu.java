451d04b163de2aceb1a51e3ebd8ffeb9983192bc
Robert Scruggs <scruggstyrese@gmail.com>
2022-11-24 00:55:10 -0500

    na

---
+++ src/CarShopMenu.java
@@ -0,0 +1,72 @@
+public class CarShopMenu {
+    //variables
+    int dentRepair = 100;
+    int paintRepair = 200;
+    int windShieldRepair = 300;
+    int bumperRepair = 400;
+    int fenderRepair = 500;
+    int total = 0;
+
+    public void displayCarMenu(){
+        System.out.println(
+                "1)Dent Repair \n" +
+                        "2)Paint Repair \n" +
+                        "3)Windshield Repair \n" +
+                        "4)Bumper Repair \n" +
+                        "5)Fender Repair \n");
+    }
+    public static void displayTotal(People people){
+        System.out.println("Total: $500");
+        System.out.println("Have a nice day " + people.name);
+    }
+
+    //getter/setters/methods
+    public void addToTotal(int menuItemPrice){
+        this.total += menuItemPrice;
+    }
+
+    public int getDentRepair() {
+        return dentRepair;
+    }
+
+    public void setDentRepair(int dentRepair) {
+        this.dentRepair = dentRepair;
+    }
+
+    public int getPaintRepair() {
+        return paintRepair;
+    }
+
+    public void setPaintRepair(int paintRepair) {
+        this.paintRepair = paintRepair;
+    }
+
+    public int getWindShieldRepair() {
+        return windShieldRepair;
+    }
+
+    public void setWindShieldRepair(int windShieldRepair) {
+        this.windShieldRepair = windShieldRepair;
+    }
+
+    public int getBumperRepair() {
+        return bumperRepair;
+    }
+
+    public void setBumperRepair(int bumperRepair) {
+        bumperRepair = bumperRepair;
+    }
+
+    public int getFenderRepair() {
+        return fenderRepair;
+    }
+
+    public void setFenderRepair(int fenderRepair) {
+        fenderRepair = fenderRepair;
+    }
+
+
+
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,14 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void displayTotal(People people){
-        System.out.println("Total: $500");
-        System.out.println("Have a nice day " + people.name);
-    }
-
     public static void main(String[] args) throws InterruptedException {
         Scanner input = new Scanner(System.in);
         Random random = new Random();
+        CarShopMenu carShopMenu = new CarShopMenu();
 
         while(true){
             People player = new People("Robert",6);
@@ -21,21 +18,44 @@ public class Main {
             People random4 = new People("Mustard",4);
             People random5 = new People("Ketchup",5);
 
+
             ArrayList<String> areas = new ArrayList<String>();
             areas.add("in the lobby");
             areas.add("outside of the building");
 
 
 
-            System.out.print("Hey welcome to our body shop! \nWhat kind of car do you have? ");
-            String carType = input.nextLine();
+            System.out.print("Hey welcome to Robert's Body Emporium! \n\nWhat can we assist you with? \n");
+            carShopMenu.displayCarMenu();
+            ArrayList <Integer> runningTotal = new ArrayList<Integer>();
+            boolean lookingAtMenu = true;
+            do{
+                for (int i = 0; i < runningTotal.size(); i++){
+                    System.out.println(runningTotal.get(i) + " \n");
+                }
+                int menuItem = input.nextInt();
+                input.nextLine();
+                runningTotal.add(menuItem);
+                System.out.print("Anything else? ");
+                String anythingElse = input.nextLine().toLowerCase();
+                if (anythingElse.equals("yes")){
+                    continue;
+                }else{
+                    break;
+                }
+
+            }while(lookingAtMenu);
+
+            for (int i = 0; i < runningTotal.size(); i++){
+                System.out.println(runningTotal.get(i));
+            }
 
             Thread.sleep(2000);
-            System.out.println("Great we will take care of your " + carType + " real nice!\nGo ahead and wait wherever you like and we will call you.");
+            System.out.println("\n");
 
             Thread.sleep(2000);
             String placeToWait = areas.get(random.nextInt(2));
-            System.out.println(player.name + " went to wait " + placeToWait);
+            System.out.println(player.name + " went to wait " + placeToWait + "\n");
 
 
             while (player.queueNumber > 0){
@@ -44,7 +64,7 @@ public class Main {
                 Thread.sleep(2000);
             }
 
-            System.out.println("You're car is finished " + player.name);
+            System.out.println("\nYou're car is finished " + player.name + "\n");
             System.out.print("Wanna check it out? \nor go ahead and pay? ");
             String checkItOut = input.nextLine().toLowerCase();
             if (checkItOut.equals("check it out")){
@@ -52,10 +72,10 @@ public class Main {
                 System.out.print("Does it look like it needs more work \nor are you ready to pay? ");
                 String toPay = input.nextLine();
                 if (toPay.equals("pay")){
-                    displayTotal(player);
+                    carShopMenu.displayTotal(player);
                 }
             }else{
-                displayTotal(player);
+                carShopMenu.displayTotal(player);
 
             }
 
